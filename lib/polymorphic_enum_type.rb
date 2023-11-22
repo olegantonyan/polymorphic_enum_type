947cb366143fec86d8576af08b161fceb05e4d68
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-11-22 09:08:33 +0100

    add instance_methods: false to enum

--- lib/polymorphic_enum_type.rb
+++ lib/polymorphic_enum_type.rb
@@ -7,9 +7,9 @@ module PolymorphicEnumType
   def belongs_to(name, scope = nil, **options)
     if options.delete(:enum_type)
       if ActiveRecord::VERSION::MAJOR >= 7
-        enum("#{name}_type", PolymorphicEnumType.config.enum_hash(name), scopes: false)
+        enum("#{name}_type", PolymorphicEnumType.config.enum_hash(name), scopes: false, instance_methods: false)
       else
-        enum("#{name}_type".to_sym => PolymorphicEnumType.config.enum_hash(name), _scopes: false)
+        enum("#{name}_type".to_sym => PolymorphicEnumType.config.enum_hash(name), _scopes: false, _instance_methods: false)
       end
     end
 
